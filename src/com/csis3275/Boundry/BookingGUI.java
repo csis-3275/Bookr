5387004960b8777e65a773015162472ea90a83fa
unknown <mdinh88@gmail.com>
2019-06-18 21:38:39 -0700

    added login and registration panel

---
+++ src/com/csis3275/Boundry/BookingGUI.java
@@ -0,0 +1,43 @@
+package com.csis3275.Boundry;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BookingGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BookingGUI window = new BookingGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BookingGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
