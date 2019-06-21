a7bf11ab5cd26de22fa59cd598603c2c8f24d35e
Gayhun <leeg23@student.douglascollege.ca>
2019-06-20 23:21:37 -0700

    Implemented rest CRUD functionality; Added UserDAO and UserDAOImpl; tested code

--- src/com/csis3275/Boundary/ReservationDAOImpl.java
+++ src/com/csis3275/Boundary/ReservationDAOImpl.java
@@ -1,5 +1,7 @@
 package com.csis3275.Boundary;
 
+import java.util.ArrayList;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -56,7 +58,7 @@ public class ReservationDAOImpl implements ReservationDAO {
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			System.err.println(ex);
+			System.err.println(ex.getMessage());
 			return false;
 		} finally {
 			session.close();
@@ -64,6 +66,83 @@ public class ReservationDAOImpl implements ReservationDAO {
 		}
 		return true;
 
+	}
+
+
+	@Override
+	public Reservation readReservation(Integer reservationId) {
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		Transaction transaction = null;
+		Reservation readReserv = null;
+		try {
+			sessionFactory = FactoryGenerator.getFactory();
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			
+			readReserv = session.get(Reservation.class, reservationId);
+			
+			transaction.commit();
+		}catch(HibernateException hx) {
+			System.err.println(hx.getMessage());
+		}finally {
+			session.close();
+			sessionFactory.close();
+		}
+		return readReserv;
+	}
+
+	@Override
+	public ArrayList<Reservation> readAllReservations() {
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		Transaction transaction = null;
+		String query = "FROM Reservation";
+		ArrayList<Reservation> reservList= null;
+		try {
+			sessionFactory = FactoryGenerator.getFactory();
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			
+			reservList = (ArrayList<Reservation>) session.createQuery(query).list();
+			
+			transaction.commit();
+		}catch(HibernateException hx) {
+			System.err.println(hx.getMessage());
+		}finally {
+			session.close();
+			sessionFactory.close();
+		}
+		return reservList;
+	}
+
+	@Override
+	public boolean deleteReservation(Reservation reservation) {
+		
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		Transaction transaction = null;
+		
+		try {
+			sessionFactory = FactoryGenerator.getFactory();
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			
+			session.delete(reservation);
+			
+			transaction.commit();
+			
+		}catch(HibernateException ex) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			System.err.println(ex.getMessage());
+			return false;
+		}finally {
+			session.close();
+			sessionFactory.close();
+		}
+		return true;
 	}
 
 }
