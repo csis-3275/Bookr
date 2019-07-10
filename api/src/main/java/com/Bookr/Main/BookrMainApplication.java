2bcd4df2cbf7716521db4aea29b3375b92cc01f4
Dei-Gee <deji2109@gmail.com>
2019-07-09 18:57:21 -0700

    first beta commit

---
+++ api/src/main/java/com/Bookr/Main/BookrMainApplication.java
@@ -0,0 +1,13 @@
+package com.Bookr.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrMainApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/Bookr/Main/BookrMainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Bookr.Main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookrMainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
