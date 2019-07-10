c77de7965d7fe41b9baca3f336553431f45903fa
Dei-Gee <deji2109@gmail.com>
2019-07-09 19:35:24 -0700

    room controller created with get mapping

--- api/src/main/java/com/Bookr/Controllers/RoomController.java
+++ api/src/main/java/com/Bookr/Controllers/RoomController.java
@@ -3,10 +3,29 @@
  */
 package com.Bookr.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Bookr.Services.RoomService;
+
 /**
- * @author lowbl
+ * @author Ayodeji Tolu-ojo 300249754
  *
  */
-public class RoomController {
 
+@RestController
+@RequestMapping("/api/rooms")
+@CrossOrigin
+public class RoomController {
+	@Autowired
+	
+	private RoomService roomService;
+	
+	@GetMapping("/all")
+	public Iterable getAllRooms(){
+		return roomService.findAll();
+	}
 }
