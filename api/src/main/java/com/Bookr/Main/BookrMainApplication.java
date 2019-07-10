bffb3e092a28c387920c861cab2957458bde9245
Dei-Gee <deji2109@gmail.com>
2019-07-09 19:14:48 -0700

    package scans added

--- api/src/main/java/com/Bookr/Main/BookrMainApplication.java
+++ api/src/main/java/com/Bookr/Main/BookrMainApplication.java
@@ -2,8 +2,14 @@ package com.Bookr.Main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.Bookr.Controllers", "com.Bookr.Services"})
+@EntityScan("com.Bookr.Entities")
+@EnableJpaRepositories("com.Bookr.Boundaries")
 public class BookrMainApplication {
 
 	public static void main(String[] args) {
