3bac4471566188ae34833405676bf36a2c8e70a0
unknown <mdinh88@gmail.com>
2019-07-21 21:27:59 -0700

    starting reservation component

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -4,47 +4,48 @@ import classnames from 'classnames';
 import Header from '../partials/Header';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Login from '../login/Login';
+import Reservation from '../reservation/Reservation';
 
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            username: "", 
+        this.state = {
+            username: "",
             password: ""
-         };
+        };
     }
 
-    handleChange(e){
+    handleChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         });
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault();
         const user = {
-            username: this.state.username, 
-            password: this.state.password 
+            username: this.state.username,
+            password: this.state.password
         };
     }
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className="main-bg">
                 <Header />
                 <Container className="cm-top" fluid={true}>
                     <Row noGutters={true} className="justify-content-between">
                         <Col sm="6">
                             <Row>
-                                <Col sm={{span: 8, offset: 2}}>
+                                <Col sm={{ span: 8, offset: 2 }}>
                                     <Row className="justify-content-center">
                                         <h1 className="text-center">You can book your room <span className="text-primary">now!</span></h1>
                                     </Row>
                                 </Col>
                             </Row>
                             <Row className="elm-top">
-                                <Col sm={{span: 10, offset: 1}}>
+                                <Col sm={{ span: 10, offset: 1 }}>
                                     <Row>
                                         <h4 className="font-weight-light text-center">
                                             We have been working very hard to create the new version of our project. It comes with a lot of new features which are easy to follow. Check it out now!
@@ -53,7 +54,7 @@ class Home extends Component {
                                 </Col>
                             </Row>
                             <Row className="elm-top">
-                                <Col sm={{span: 10, offset: 1}}>
+                                <Col sm={{ span: 10, offset: 1 }}>
                                     <Row className="justify-content-center">
                                         <h4 className="font-weight-light text-center">
                                             Click the button below!
@@ -62,7 +63,7 @@ class Home extends Component {
                                 </Col>
                             </Row>
                             <Row className="elm-top">
-                                <Col sm={{span: 10, offset: 1}}>
+                                <Col sm={{ span: 10, offset: 1 }}>
                                     <Row className="justify-content-center">
                                         <Button className="py-2 px-5">Register</Button>
                                     </Row>
@@ -71,13 +72,13 @@ class Home extends Component {
 
                         </Col>
                         <Col sm="5">
-                            <Login />
+                            <Reservation />
                         </Col>
                     </Row>
                 </Container>
             </div>
-            );
+        );
     }
 }
- 
+
 export default Home;
---
+++ app/src/components/reservation/Reservation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Column, Row } from 'simple-flexbox';
+
+
+export default class Reservation extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Reservations</h4>
+                <span>
+
+                </span>
+            </div>
+        )
+    }
+}
