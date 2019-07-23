c15ac9213839cc7e4e29ae3058d7fb624a196967
unknown <mdinh88@gmail.com>
2019-07-22 22:01:56 -0700

    adding header and background to profile

--- app/src/components/profile/Profile.js
+++ app/src/components/profile/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Form, InputGroup, Button, Col } from 'react-bootstrap';
+import Header from '../partials/Header';
+import '../home/styles/styles.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Profile extends Component {
 
@@ -24,45 +26,57 @@ class Profile extends Component {
 
     render() {
         return (
-            <form className="bg-light py-5 px-5 rounded">
-                <input
-                    name="firstName"
-                    placeholder="First Name"
-                    value={this.state.firstName}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="lastName"
-                    placeholder="Last Name"
-                    value={this.state.lastName}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="email"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="phone"
-                    placeholder="Phone Number"
-                    value={this.state.phone}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={e => this.change(e)}
-                />
-                <br />
-                <button onClick={e => this.onSubmit(e)}>Update</button>
-            </form>
+            <div div className="main-bg" fluid={true}>
+                <Header />
+                <Container className="cm-top" fluid={true}>
+                    <Row className="elm-top">
+                        <Col sm={{ span: 10, offset: 1 }}>
+                            <Row className="justify-content-center">
+                                <form className="bg-light py-5 px-5 rounded">
+                                    <h1 className="text-dark">Update Profile</h1>
+                                    <input
+                                        name="firstName"
+                                        placeholder="First Name"
+                                        value={this.state.firstName}
+                                        onChange={e => this.change(e)}
+                                    />
+                                    <br />
+                                    <input
+                                        name="lastName"
+                                        placeholder="Last Name"
+                                        value={this.state.lastName}
+                                        onChange={e => this.change(e)}
+                                    />
+                                    <br />
+                                    <input
+                                        name="email"
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        onChange={e => this.change(e)}
+                                    />
+                                    <br />
+                                    <input
+                                        name="phone"
+                                        placeholder="Phone Number"
+                                        value={this.state.phone}
+                                        onChange={e => this.change(e)}
+                                    />
+                                    <br />
+                                    <input
+                                        name="password"
+                                        type="password"
+                                        placeholder="Password"
+                                        value={this.state.password}
+                                        onChange={e => this.change(e)}
+                                    />
+                                    <br />
+                                    <button onClick={e => this.onSubmit(e)}>Update</button>
+                                </form>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
