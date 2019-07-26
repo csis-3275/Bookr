bd3a214126a15b3e63e2b39119b0eeb9bfafc00d
Gayhun <leeg23@student.douglascollege.ca>
2019-07-26 00:47:17 -0700

    Nav Component Updated

--- app/src/components/partials/Header.js
+++ app/src/components/partials/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
+
 class Header extends Component {
     constructor(props) {
         super(props);
@@ -9,25 +10,21 @@ class Header extends Component {
     render() { 
         return ( 
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="/">Bookr.com</Navbar.Brand>
+                <Navbar.Brand href="/" className="text-info pl-2 font_big">Bookr</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets" className="text-info pr-4">email@email.com</Nav.Link>
+                        <NavDropdown title="Dashboard" id="collasible-nav-dropdown" className="pr-5">
+                            <NavDropdown.Item href="#action/3.1">Book Rooms</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Update Reservation</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Cancel Booking</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
