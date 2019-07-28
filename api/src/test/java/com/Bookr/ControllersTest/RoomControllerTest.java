c7fc944df7d37c978239dce71f66beb8d42624ae
Gayhun <leeg23@student.douglascollege.ca>
2019-07-28 16:51:20 -0700

    RoomControllerTest Modified, import optimized

--- api/src/test/java/com/Bookr/ControllersTest/RoomControllerTest.java
+++ api/src/test/java/com/Bookr/ControllersTest/RoomControllerTest.java
@@ -8,21 +8,13 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,14 +22,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.Bookr.Controllers.ReservationController;
 import com.Bookr.Controllers.RoomController;
-import com.Bookr.Controllers.UserController;
-import com.Bookr.Entities.Reservation;
 import com.Bookr.Entities.Room;
-import com.Bookr.Entities.User;
 import com.Bookr.Services.RoomService;
-import com.Bookr.Services.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -112,7 +99,7 @@ public class RoomControllerTest {
 		
 		String expectedJson = this.mapToJson(roomList);
 		String outputInJson = result.getResponse().getContentAsString();
-		
+				
 		assertEquals(expectedJson, outputInJson);
 	}
 
