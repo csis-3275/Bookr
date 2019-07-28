d3e00be0db154352153b89482bf1c86033d5bf0f
Gayhun <leeg23@student.douglascollege.ca>
2019-07-28 16:52:42 -0700

    User controller test: getByUserId, getAll test implemented

--- api/src/test/java/com/Bookr/ControllersTest/UserControllerTest.java
+++ api/src/test/java/com/Bookr/ControllersTest/UserControllerTest.java
@@ -5,20 +5,17 @@ package com.Bookr.ControllersTest;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,10 +24,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.Bookr.Controllers.ReservationController;
-import com.Bookr.Controllers.RoomController;
 import com.Bookr.Controllers.UserController;
-import com.Bookr.Entities.Reservation;
 import com.Bookr.Entities.User;
 import com.Bookr.Services.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -77,6 +71,70 @@ public class UserControllerTest {
 		
 		assertEquals(outputInJson, inputInJson);
 		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+	}
+
+
+	@Test
+	public void testGetUserById() throws Exception {
+		
+		User user = new User();
+		user.set_role_id(1);
+		user.set_firstname("Amber");
+		user.set_lastname("Mclean");
+		user.set_email("email@email.com");
+		user.set_password("passwords");
+		
+		String URI = "/api/users/1";
+		
+		Mockito.when(userService.getById(Mockito.anyInt())).thenReturn(user);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get(URI)
+				.accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		
+		String expectedJson = this.mapToJson(user);
+		String outputInJson = result.getResponse().getContentAsString();
+		
+		assertEquals(expectedJson, outputInJson);
+	}
+	
+	@Test
+	public void testgetAllRooms() throws Exception {
+		
+		User user1 = new User();
+		user1.set_role_id(1);
+		user1.set_firstname("Amber");
+		user1.set_lastname("Mclean");
+		user1.set_email("email@email.com");
+		user1.set_password("passwords");
+		
+		User user2 = new User();
+		user2.set_role_id(1);
+		user2.set_firstname("Anuar");
+		user2.set_lastname("Muckhambetzhanov");
+		user2.set_email("Anuar@email.com");
+		user2.set_password("passwords12");
+		
+		List<User> userList = new ArrayList<>();
+		userList.add(user1);
+		userList.add(user2);
+		
+		Mockito.when(userService.findAll()).thenReturn(userList);
+		
+		String URI = "/api/users/all";
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get(URI)
+				.accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		
+		String expectedJson = this.mapToJson(userList);
+		String outputInJson = result.getResponse().getContentAsString();
+		
+		assertEquals(expectedJson, outputInJson);
 	}
 
 	private String mapToJson(Object object) throws JsonProcessingException {
