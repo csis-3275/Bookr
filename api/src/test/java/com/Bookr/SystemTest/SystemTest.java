558b383b4ff776bb92f9aa1a51d2edce483e8963
Gayhun <leeg23@student.douglascollege.ca>
2019-07-29 22:25:18 -0700

    SystemTest Class created

---
+++ api/src/test/java/com/Bookr/SystemTest/SystemTest.java
@@ -0,0 +1,17 @@
+package com.Bookr.SystemTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * @author Gahyun Lee 300289068
+ *
+ */
+public class SystemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
