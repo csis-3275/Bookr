f6f7924d431482abbd04cf2f0b341931a581bacb
Gayhun <leeg23@student.douglascollege.ca>
2019-07-30 20:19:44 -0700

    temp commit to pull and merge new develop_beta branch

--- api/src/test/java/com/Bookr/SystemTest/SystemTest.java
+++ api/src/test/java/com/Bookr/SystemTest/SystemTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 /**
  * @author Gahyun Lee 300289068
- *
+ * System Test - A System test using JUnit for the CRUDoperations for at least one Entity class. 
+ * You must use the same Database Access class that your main application uses
  */
 public class SystemTest {
 
